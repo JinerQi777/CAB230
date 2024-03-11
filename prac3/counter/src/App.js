d4a2ed3491eb1908c37a1a9afe5f449dedbe74da
Jiner Qi <jinerqi77@gmail.com>
2024-03-12 07:46:07 +1000

    starting point of week 3 prac

---
+++ prac3/counter/src/App.js
@@ -0,0 +1,22 @@
+// Adding Some Functionality useState 
+
+// make a like counter 
+
+//make a reuseable component , Headline include a title and like counter
+
+//Using fetch to Load Content  from an API ( useEffect next week )
+
+
+
+
+
+function App() {
+  return (
+    <div >
+      <h1>Welcome week 3!</h1>
+    
+    </div>
+  );
+}
+
+export default App;
